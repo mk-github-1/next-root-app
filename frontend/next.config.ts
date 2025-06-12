9ad6a968cc2eaf0a293bcd997d3b6ed67960bb2d
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2025-06-12 21:24:07 +0900

    Update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* プレフィックス NEXT_PUBLIC_ or SECRET_ */
+  env: {
+    // この設定はGitHub公開用で優先して選択されます
+    // 通常は.envファイルに記載してください (GitHubへのプッシュは除外されます)
+    NEXT_PUBLIC_API_URL: "http://localhost:3000/api"
+  }
 };
 
 export default nextConfig;
